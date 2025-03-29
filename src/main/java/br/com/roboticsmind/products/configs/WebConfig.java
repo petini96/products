cb8ef1519590eb2534edf9d71cf84ebc25885ebb
Vinícius <petiniprojetos96@gmail.com>
2025-03-28 21:40:40 -0400

    added google variable and change cors

--- src/main/java/br/com/roboticsmind/products/configs/WebConfig.java
+++ src/main/java/br/com/roboticsmind/products/configs/WebConfig.java
@@ -13,8 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
+                        "*",
                         "http://localhost:5173",
-                        "https://importsjpe.roboticsmind.com.br"
+                        "https://artesanaldoceria.roboticsmind.com.br"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
